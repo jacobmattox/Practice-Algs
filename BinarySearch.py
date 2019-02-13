33376c31ee1c7020c2f70a902b24e3b12fb0743d
jacobmattox <jacob.mattox1017@gmail.com>
2019-02-12 19:22:39 -0500

    fixed binary search iterative

--- BinarySearch.py
+++ BinarySearch.py
@@ -3,18 +3,26 @@
 #requires sorted list and key to search for
 def binarySearch(sortedList, key):
 
-    middle = len(sortedList)//2
+    middle = (len(sortedList) - 1)//2
+    left = 0
+    right = len(sortedList)
+    while key != sortedList[middle] and left < right:
+        if key < sortedList[middle]:
+            right = middle - 1
+            middle = (left + right) // 2
+        else:
+            left = middle + 1
+            middle = (left + right) // 2
     if key == sortedList[middle]:
-        return True
-    elif key > sortedList[middle]:
-        return binarySearch(sortedList[middle + 1: ], key)
+        return middle
+
     else:
-        return binarySearch(sortedList[:middle], key)
-    return False
+        return False
+
 
 def main():
     listToSearch = [2, 6, 7, 9, 12, 13, 15, 17, 18, 21, 24, 29, 32, 35]
-    keyToFind = 24
+    keyToFind = 9
 
     print(binarySearch(listToSearch, keyToFind))
 
